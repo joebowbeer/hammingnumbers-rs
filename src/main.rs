bcd1e91d0431c44e24476cab4497288873c786f5
Joe Bowbeer <joe.bowbeer@gmail.com>
2024-07-07 23:03:52 -0700

    source code!

---
+++ src/main.rs
@@ -0,0 +1,42 @@
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
+use std::env::args;
+use std::iter;
+
+/// Prints the first several Hamming numbers using a <tt>BinaryHeap</tt>.
+fn main() {
+    let n: usize = args()
+        .nth(1)
+        .and_then(|a| a.parse().ok())
+        .expect("error: length required");
+    println!("{:?}", hamming_iter().take(n).collect::<Vec<_>>());
+}
+
+fn hamming_iter() -> impl Iterator<Item = usize> {
+    let mut q = BinaryHeap::new();
+    q.push(Reverse(1));
+
+    iter::from_fn(move || {
+        let Reverse(i) = q.pop().unwrap();
+        while let Some(Reverse(p)) = q.peek() {
+            if *p != i {
+                break;
+            }
+            q.pop();
+        }
+        q.push(Reverse(2 * i));
+        q.push(Reverse(3 * i));
+        q.push(Reverse(5 * i));
+        Some(i)
+    })
+}
+
+#[test]
+fn hamming_iter_test() {
+    assert_eq!(hamming_iter().take(0).collect::<Vec<_>>(), []);
+    assert_eq!(hamming_iter().take(1).collect::<Vec<_>>(), [1]);
+    assert_eq!(
+        hamming_iter().take(10).collect::<Vec<_>>(),
+        [1, 2, 3, 4, 5, 6, 8, 9, 10, 12]
+    );
+}
